da9bdc5a6912e5d278d16a9bf69525be5e1bd0e9
Vitor Fel <vitorfel2000@gmail.com>
2022-09-13 19:04:23 -0300

    :sparkles: Criando posições randômicas

--- game.js
+++ game.js
@@ -1,12 +1,30 @@
-var height = 0;
 var width = 0;
+var height = 0;
+var positionX;
+var positionY;
+var mosquito;
 
 function adjustsStageSizeGame() {
   height = window.innerHeight;
   width = window.innerWidth;
-
-  console.log(height);
-  console.log(width);
 }
 
 adjustsStageSizeGame();
+
+function randomPosition() {
+  positionX = Math.floor(Math.random() * width) - 90;
+  positionY = Math.floor(Math.random() * height) - 90;
+
+  positionX = (positionX < 0 ? 0 : positionX);
+  positionY = (positionY < 0 ? 0 : positionY);
+
+  console.log(positionX, positionY);
+
+  mosquito = document.createElement("img");
+  mosquito.src = "assets/mosca.png";
+  mosquito.className = "mosca";
+  mosquito.style.left = positionX + 'px';
+  mosquito.style.top = positionY + 'px';
+  mosquito.style.position = 'absolute';
+  document.body.appendChild(mosquito);
+}
