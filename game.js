71e213c3191f3e4b26c5281cd6c9502dc59a3ddf
Vitor Fel <vitorfel2000@gmail.com>
2022-09-12 21:19:17 -0300

    :sparkles: Definindo a dimensão do palco do jogo

---
+++ game.js
@@ -0,0 +1,12 @@
+var height = 0;
+var width = 0;
+
+function adjustsStageSizeGame() {
+  height = window.innerHeight;
+  width = window.innerWidth;
+
+  console.log(height);
+  console.log(width);
+}
+
+adjustsStageSizeGame();
