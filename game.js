5513123ca4b9f8908368b72d992eff200b32c4d8
Vitor Fel <vitorfel2000@gmail.com>
2022-09-13 19:15:18 -0300

    :sparkles: Tamanhos randômicos

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
 var width = 0;
 var height = 0;
+
 var positionX;
 var positionY;
+
 var mosquito;
+var sizeMosquito;
 
 function adjustsStageSizeGame() {
   height = window.innerHeight;
@@ -15,16 +18,29 @@ function randomPosition() {
   positionX = Math.floor(Math.random() * width) - 90;
   positionY = Math.floor(Math.random() * height) - 90;
 
-  positionX = (positionX < 0 ? 0 : positionX);
-  positionY = (positionY < 0 ? 0 : positionY);
-
-  console.log(positionX, positionY);
+  positionX = positionX < 0 ? 0 : positionX;
+  positionY = positionY < 0 ? 0 : positionY;
 
   mosquito = document.createElement("img");
   mosquito.src = "assets/mosca.png";
-  mosquito.className = "mosca";
-  mosquito.style.left = positionX + 'px';
-  mosquito.style.top = positionY + 'px';
-  mosquito.style.position = 'absolute';
+  mosquito.className = randomSize();
+  mosquito.style.left = positionX + "px";
+  mosquito.style.top = positionY + "px";
+  mosquito.style.position = "absolute";
   document.body.appendChild(mosquito);
+
+  randomSize();
+}
+
+function randomSize() {
+  sizeMosquito = Math.floor(Math.random() * 3);
+
+  switch (sizeMosquito) {
+    case 0:
+      return 'mosquitoSmall'
+    case 1:
+      return 'mosquitoNormal'
+    case 2:
+      return 'mosquitoLarge'
+  }
 }
